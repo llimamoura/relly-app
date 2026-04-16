3ade08f0df08a75f8cca25da0a58a97004420a8b
Gabriel Lima <gabriellglma9@gmail.com>
2026-04-16 03:27:39 -0300

    config Hilt module

--- app/src/main/java/com/relly/app/di/RepositoryModule.kt
+++ app/src/main/java/com/relly/app/di/RepositoryModule.kt
@@ -1,4 +1,20 @@
 package com.relly.app.di
 
-class RepositoryModule {
+import com.relly.app.data.repository.AuthRepositoryImpl
+import com.relly.app.domain.repository.AuthRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        impl: AuthRepositoryImpl
+    ): AuthRepository
 }
